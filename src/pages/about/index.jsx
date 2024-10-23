3ed9d80384777841694f3fcbd51a5ad2ffd35cc7
MTruong2000 <truongdev2000@gmail.com>
2024-10-23 13:06:01 +0700

    setup react router

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './style.scss'
+
+function About() {
+  return (
+    <>
+      ABOUT PAGES
+    </>
+  )
+}
+
+export default About
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './style.scss'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      HOME PAGES
+    </>
+  )
+}
+
+export default Home
